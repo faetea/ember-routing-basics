51195c35d9e916fbbd1188ac63ce1e634d46302d
saphron <saphron.scul@gmail.com>
2015-12-08 11:10:36 -0500

    added team sub-routes to team and contact

--- app/router.js
+++ app/router.js
@@ -7,6 +7,15 @@ const Router = Ember.Router.extend({
 
 Router.map(function() {
   this.route('about');
+  this.route('team', function(){
+    this.route('engineering');
+    this.route('leadership');
+    this.route('sales');
+  });
+  this.route('contact', function(){
+    this.route('boston');
+    this.route('newyork');
+  });
 });
 
 export default Router;
